138235762ae15ea067cf085531719b398bfb5a11
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-14 11:35:58 +0530

    Update OrderResource.php

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -13,6 +13,11 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use App\Models\Product;
+
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
@@ -23,12 +28,61 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('total_amount')
                     ->required()
                     ->numeric(),
-                Forms\Components\TextInput::make('total_price')
-                    ->required()
-                    ->numeric(),
+                Forms\Components\Repeater::make('orderItems')
+                    ->relationship()
+                    ->schema([
+                        Forms\Components\Select::make('product_id')
+                            ->relationship('product', 'name')
+                            ->live(debounce:500)
+                            ->afterStateUpdated(function(Set $set, Get $get){
+
+                                $product = Product::where('id', $get('product_id'))->first();
+
+                                // dd($product);
+
+                                $unit_price = $product ? $product->price : 0;
+                                
+                                $set('unit_price', $unit_price);
+                                
+                                // dd($get('../../payment_method'));                    
+                                
+                                self::updateOrderItemAmount($set, $get);
+                                self::updateOrderTotal($set, $get);
+
+                            })
+                            ->required(),
+                        Forms\Components\TextInput::make('qty')
+                            ->required()
+                            ->numeric()
+                            ->default(1)
+                            ->live(debounce:500)
+                            ->afterStateUpdated(function(Set $set, Get $get){
+                                self::updateOrderItemAmount($set, $get);
+                                // self::updateOrderTotal($set, $get);
+                            }),
+                        Forms\Components\TextInput::make('unit_price')
+                            ->required()
+                            ->numeric()
+                            ->disabled()
+                            ->live(debounce:500)
+                            ->afterStateUpdated(function(Set $set, Get $get){
+                                self::updateOrderItemAmount($set, $get);
+                                // self::updateOrderTotal($set, $get);
+                            }),
+                        Forms\Components\TextInput::make('sub_total')
+                            ->required()
+                            ->disabled()
+                            ->numeric(),
+                    ])
+                    ->columns(4)
+                    ->columnSpanFull()
+                        
             ]);
     }
 
@@ -36,7 +90,7 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
+                Tables\Columns\TextColumn::make('user')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('total_price')
@@ -78,5 +132,43 @@ class OrderResource extends Resource
             'create' => Pages\CreateOrder::route('/create'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
         ];
+    }
+
+    protected static function updateOrderItemAmount(Set $set, Get $get)
+    {
+        $qty = (int) $get('qty');
+        $unit_price = (int) $get('unit_price');
+        // $discount = (int) $get('discount');
+
+        $amount = $qty * $unit_price;
+        // if ($discount > 0) {
+        //     $amount -= ($amount * $discount / 100);
+        // }
+        $set('sub_total', $amount);
+    }
+
+    protected static function updateOrderTotal(Set $set, Get $get)
+    {
+        $totalAmount = 0;
+
+        // dd($get('../../total_amount'));
+        // dd($get('../../orderItems'));
+        
+        // Default to an empty array if null
+        $orderItems = $get('../../orderItems') ?? [];
+        
+        // Debugging to check the value of $orderItems
+        // dd($orderItems);
+
+        // Ensure $orderItems is an array before using array_reduce
+        if (is_array($orderItems)) {
+            $total_amount = array_reduce($orderItems, function($carry, $item) {
+                return $carry + ($item['sub_total'] ?? 0);
+            }, 0);
+            $set('../../total_amount', $total_amount);
+        } else {
+            // Handle the case where $orderItems is not an array
+            $set('../../total_amount', 0);
+        }
     }
 }
