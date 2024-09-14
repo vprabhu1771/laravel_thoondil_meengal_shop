b866c267b3002740b702b5873d8d4adf45f90f99
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-14 10:57:11 +0530

    Update database seeder for products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Facades\Hash;
 
+use App\Models\Product;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -22,5 +24,89 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
         ]);
+    
+
+    $products = [
+        [
+            'name' => 'பாறை மீன் வறுவல்',
+            'price' => 60,
+            'unit' => '1'
+            
+        ],
+        [
+            'name' => 'வஞ்ஐரம் மீன் வறுவல்',
+            'price' => 150,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'இறால் 65',
+            'price' => 100,
+            'unit' => '100'
+        ],
+        [
+            'name' => 'நெத்திலி 65 ',
+            'price' => 70,
+            'unit' => '100'
+        ],
+        [
+            'name' => 'இறால் பிரை',
+            'price' => 80,
+            'unit' => '100'
+        ],
+        [
+            'name' => 'நெத்திலி பிரை ',
+            'price' => 60,
+            'unit' => '100'
+        ],
+        [
+            'name' => 'இறால்/கனவா தொக்கு',
+            'price' => 60,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'இறால்/கனவா தோசை',
+            'price' => 30,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'தோசை & மீன் குழம்பு',
+            'price' => 10,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'இட்லி(2) மீன் குழம்பு',
+            'price' => 15,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'சப்பாத்தி ',
+            'price' => 15,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'நண்டு சூப்',
+            'price' => 40,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'பார்சல் நண்டு சூப் ',
+            'price' => 50,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'மீன் குழம்பு ',
+            'price' => 40,
+            'unit' => '1'
+        ],
+        [
+            'name' => 'நண்டு கிரெவி',
+            'price' => 50,
+            'unit' => '1'
+        ],
+       
+    ];
+        foreach ($products as $row){
+            Product::create($row);
+        }
     }
 }
