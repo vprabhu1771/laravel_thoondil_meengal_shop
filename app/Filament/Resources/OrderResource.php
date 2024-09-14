cf1d921fcbd37c47006bc1406e8dabd90dc4eb84
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-14 13:14:17 +0530

    Update OrderResource.php

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -18,6 +18,8 @@ use App\Models\Product;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
 
+use Filament\Forms\Components\Actions\Action;
+
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
@@ -36,6 +38,21 @@ class OrderResource extends Resource
                     ->numeric(),
                 Forms\Components\Repeater::make('orderItems')
                     ->relationship()
+                    // Repeatable field is live so that it will trigger the state update on each change
+                    ->live(debounce:500)
+                    // After adding a new row, we need to update the totals
+                    ->afterStateUpdated(function (Set $set, Get $get) {
+                        self::updateOrderItemAmount($set, $get);
+                        self::updateOrderTotal($set, $get);
+                    })
+                    // After deleting a row, we need to update the totals
+                    ->deleteAction(
+                        fn(Action $action) => $action->after(function (Set $set, Get $get) {
+                            self::updateOrderItemAmount($set, $get);
+                            self::updateOrderTotal($set, $get);
+                            // dd();
+                        })
+                    )
                     ->schema([
                         Forms\Components\Select::make('product_id')
                             ->relationship('product', 'name')
@@ -64,16 +81,17 @@ class OrderResource extends Resource
                             ->live(debounce:500)
                             ->afterStateUpdated(function(Set $set, Get $get){
                                 self::updateOrderItemAmount($set, $get);
-                                // self::updateOrderTotal($set, $get);
+                                self::updateOrderTotal($set, $get);
                             }),
                         Forms\Components\TextInput::make('unit_price')
                             ->required()
                             ->numeric()
+                            ->prefix('ரூ')
                             ->disabled()
                             ->live(debounce:500)
                             ->afterStateUpdated(function(Set $set, Get $get){
-                                self::updateOrderItemAmount($set, $get);
-                                // self::updateOrderTotal($set, $get);
+                                // self::updateOrderItemAmount($set, $get);
+                                self::updateOrderTotal($set, $get);
                             }),
                         Forms\Components\TextInput::make('sub_total')
                             ->required()
@@ -134,41 +152,75 @@ class OrderResource extends Resource
         ];
     }
 
-    protected static function updateOrderItemAmount(Set $set, Get $get)
-    {
-        $qty = (int) $get('qty');
-        $unit_price = (int) $get('unit_price');
-        // $discount = (int) $get('discount');
+    // protected static function updateOrderItemAmount(Set $set, Get $get)
+    // {
+    //     $qty = (int) $get('qty');
+    //     $unit_price = (int) $get('unit_price');
+    //     // $discount = (int) $get('discount');
 
-        $amount = $qty * $unit_price;
-        // if ($discount > 0) {
-        //     $amount -= ($amount * $discount / 100);
-        // }
-        $set('sub_total', $amount);
+    //     $amount = $qty * $unit_price;
+    //     // if ($discount > 0) {
+    //     //     $amount -= ($amount * $discount / 100);
+    //     // }
+    //     $set('sub_total', $amount);
+    // }
+
+    // protected static function updateOrderTotal(Set $set, Get $get)
+    // {
+    //     $totalAmount = 0;
+
+    //     // dd($get('../../total_amount'));
+    //     // dd($get('../../orderItems'));
+        
+    //     // Default to an empty array if null
+    //     $orderItems = $get('../../orderItems') ?? [];
+        
+    //     // Debugging to check the value of $orderItems
+    //     // dd($orderItems);
+
+    //     // Ensure $orderItems is an array before using array_reduce
+    //     if (is_array($orderItems)) {
+    //         $total_amount = array_reduce($orderItems, function($carry, $item) {
+    //             return $carry + ($item['sub_total'] ?? 0);
+    //         }, 0);
+    //         $set('../../total_amount', $total_amount);
+    //     } else {
+    //         // Handle the case where $orderItems is not an array
+    //         $set('../../total_amount', 0);
+    //     }
+    // }
+
+    public static function updateOrderItemAmount(Set $set, Get $get): void
+    {
+        // Retrieve all selected order items and remove empty rows
+        $selectedOrderItems = collect($get('orderItems'))->filter(fn($item) => !empty($item['product_id']) && !empty($item['qty']));
+        
+        // Retrieve prices for all selected products
+        $prices = Product::find($selectedOrderItems->pluck('product_id'))->pluck('price', 'id');
+
+        // Calculate subtotal for each order item
+        $subtotals = $selectedOrderItems->map(function($item) use ($prices) {
+            return $prices[$item['product_id']] * $item['qty'];
+        });
+
+        // Update the sub_total for each order item
+        $selectedOrderItems->each(function ($item, $index) use ($set, $subtotals) {
+            $set("orderItems.$index.sub_total", number_format($subtotals[$index], 2, '.', ''));
+        });
     }
 
-    protected static function updateOrderTotal(Set $set, Get $get)
+    public static function updateOrderTotal(Set $set, Get $get): void
     {
-        $totalAmount = 0;
+        // Retrieve all selected order items and remove empty rows
+        $selectedOrderItems = collect($get('orderItems'))->filter(fn($item) => !empty($item['product_id']) && !empty($item['qty']));
 
-        // dd($get('../../total_amount'));
-        // dd($get('../../orderItems'));
-        
-        // Default to an empty array if null
-        $orderItems = $get('../../orderItems') ?? [];
-        
-        // Debugging to check the value of $orderItems
-        // dd($orderItems);
+        // Calculate total based on the subtotal for all items
+        $totalAmount = $selectedOrderItems->reduce(function ($total, $item) {
+            return $total + $item['sub_total'];
+        }, 0);
 
-        // Ensure $orderItems is an array before using array_reduce
-        if (is_array($orderItems)) {
-            $total_amount = array_reduce($orderItems, function($carry, $item) {
-                return $carry + ($item['sub_total'] ?? 0);
-            }, 0);
-            $set('../../total_amount', $total_amount);
-        } else {
-            // Handle the case where $orderItems is not an array
-            $set('../../total_amount', 0);
-        }
+        // Update the state with the new total value
+        $set('total_amount', number_format($totalAmount, 2, '.', ''));
     }
+
 }
