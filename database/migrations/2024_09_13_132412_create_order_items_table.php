872043a9ae9b7f69262f4b492cffafe10aa01cc2
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-14 13:14:07 +0530

    renaming orderitem migration
    renaming Orderitem to order_item

--- database/migrations/2024_09_13_132412_create_order_items_table.php
+++ database/migrations/2024_09_13_132412_create_order_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orderitems', function (Blueprint $table) {
+        Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('order_id');
             $table->unsignedBigInteger('product_id');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orderitems');
+        Schema::dropIfExists('order_items');
     }
 };
