24f686e4f7836f58b1ba093ffe080fe2892c39ad
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-16 18:42:32 +0530

    api v2 Product Controller with route

---
+++ app/Http/Controllers/api/v2/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\api\v2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+        $products = Product::orderBy('name', 'asc')->get();
+
+        $transformedProducts = $products->map(function($product){
+            return[
+                'name' => $product->name,
+                'price' => $product->price,
+                'unit' => $product->unit
+            ];
+        });
+
+        return response()->json(['data' => $transformedProducts],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+use App\Http\Controllers\api\v2\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
