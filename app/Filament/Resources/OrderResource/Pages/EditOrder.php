315741a7f8099e71a08e9ff525480d6bcc8cc8cd
vprabhu1771 <vprabhu1771@gmail.com>
2024-11-18 15:20:44 +0530

    Print Logic added to edit order resource

--- app/Filament/Resources/OrderResource/Pages/EditOrder.php
+++ app/Filament/Resources/OrderResource/Pages/EditOrder.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\OrderResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
+use App\Models\Order;
+
 class EditOrder extends EditRecord
 {
     protected static string $resource = OrderResource::class;
@@ -14,6 +16,9 @@ class EditOrder extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\Action::make('Print')
+                    ->url(fn (Order $record): string => route('receipt.print', ['id' => $record->id]))
+                    ->openUrlInNewTab(),
         ];
     }
 }
