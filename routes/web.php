9002c9e516c763ae3f1c9bd7dee9af9e2e9e9534
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-29 18:21:55 +0530

    MigrationController routes

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 use App\Http\Controllers\ReceiptController;
 
 Route::get('/receipt/{id}', [ReceiptController::class, 'printReceipt'])->name('receipt.print');
+
+use App\Http\Controllers\MigrationController;
+
+Route::get('/migrate-fresh', [MigrationController::class, 'runMigrationFresh']);
+
+Route::get('/migrate', [MigrationController::class, 'runMigrations']);
+
+Route::get('/db-seed', [MigrationController::class, 'runDbSeed']);
